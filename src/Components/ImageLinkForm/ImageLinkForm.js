a2f5a6b65cb0a306d4feeb60b80a40e3c28e198d
Steven NAVE <stevennave@gmail.com>
2022-11-07 00:56:15 -0500

    imageLinkForm user fix

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({onInputChange,onButtonSubmit}) => {
 	return(
 		<div>
 		  <p className ='f3 courier new'>
-             {'This Magic Brain will detect faces in your pictures. Give it a try!'}
+             {'This Magic Brain will detect faces in pictures. Enter an image URL address to give it a try!'}
 		  </p>
 		  <div className='center'>
 		    <div className=' form center pa4 br3 shadow-5'>
