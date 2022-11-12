f236f449f2717a6457c23bc35ca8194d8a1bb9a4
Steven NAVE <stevennave@gmail.com>
2022-11-12 16:28:22 -0500

    navigation update

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -4,7 +4,7 @@ const Navigation = ({onRouteChange,isSignedIn}) => {
      if(isSignedIn){
     return(
     <nav style= {{display: 'flex', justifyContent: 'flex-end'}}>
-      <p onClick={() => onRouteChange('SignIn')}className='f3 link dim black underline pa3 pointer'> Sign Out</p>
+      <p onClick={() => onRouteChange('SignOut')}className='f3 link dim black underline pa3 pointer'> Sign Out</p>
     </nav>
     );
   } else{
