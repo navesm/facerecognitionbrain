f71d1e09e27fb689e6be7cffa42731659948a33e
Steven NAVE <stevennave@gmail.com>
2023-01-17 14:41:29 -0500

    update

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,31 +1,31 @@
 import React from 'react';
 
-class Register extends React.Component{
-	constructor() {
-		super();
-		this.state = {
-			email: '',
-			password: '',
+class Register extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      email: '',
+      password: '',
       name: ''
-		}
-	}
-
-  onNameChange = (event) => {
-    this.setState({name: event.target.value})
+    }
   }
 
-	onEmailChange = (event) => {
-		this.setState({email : event.target.value})
-	}
+  onNameChange = (event) => {
+    this.setState({ name: event.target.value })
+  }
 
-	onPasswordChange = (event) => {
-		this.setState({password: event.target.value})
-	}
+  onEmailChange = (event) => {
+    this.setState({ email: event.target.value })
+  }
 
-	onSubmitSignIn = () => {
-		fetch(' https://salty-mesa-42988.herokuapp.com/register', {
+  onPasswordChange = (event) => {
+    this.setState({ password: event.target.value })
+  }
+
+  onSubmitSignIn = () => {
+    fetch(' https://salty-mesa-42988.herokuapp.com/register', {
       method: 'post',
-      headers: {'Content-Type': 'application/json'},
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         email: this.state.email,
         password: this.state.password,
@@ -34,15 +34,15 @@ class Register extends React.Component{
     })
       .then(response => response.json())
       .then(user => {
-        if(user.id){
+        if (user.id) {
           this.props.loadUser(user);
           this.props.onRouteChange('home');
         }
       })
-	}
+  }
 
-  render(){
-    return(
+  render() {
+    return (
       <article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
           <div className="measure">
@@ -50,41 +50,41 @@ class Register extends React.Component{
               <legend className="f1 fw6 ph0 mh0">Register</legend>
               <div className="mt3">
                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Name</label>
-                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                type="text" 
-                name="name"  
-                id="name"
-                onChange = {this.onNameChange}
+                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="text"
+                  name="name"
+                  id="name"
+                  onChange={this.onNameChange}
                 />
               </div>
               <div className="mt3">
                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                <input 
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                  type="email" 
-                  name="email-address"  
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="email"
+                  name="email-address"
                   id="email-address"
-                  onChange = {this.onEmailChange}
+                  onChange={this.onEmailChange}
                 />
               </div>
               <div className="mv3">
                 <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                <input 
-                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-                  type="password" 
-                  name="password"  
+                <input
+                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  type="password"
+                  name="password"
                   id="password"
-                  onChange = {this.onPasswordChange}
+                  onChange={this.onPasswordChange}
                 />
               </div>
-              </fieldset>
-                <div className="">
-                <input 
-                onClick = {this.onSubmitSignIn}
+            </fieldset>
+            <div className="">
+              <input
+                onClick={this.onSubmitSignIn}
                 className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
-                type="submit" 
-                value="Register"/>
-              </div>
+                type="submit"
+                value="Register" />
+            </div>
           </div>
         </main>
       </article>
